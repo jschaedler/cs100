b62fbfc8533b358298cdbe6f4846cc1c3fefe467
jschaedler <jschaedler7@gmail.com>
2020-12-31 14:30:00 -0500

    add assignment 3

---
+++ cs100assign3.py
@@ -0,0 +1,12 @@
+x = float(input('Enter x'))
+y = float(input('Enter y'))
+if x >= 0:
+    if y >= 0:
+        print(x + y)
+    else:
+        print(x + y ** 2)
+else:
+    if y >= 0:
+        print(x ** 2 + y)
+    else:
+        print(x**2+y**2)
